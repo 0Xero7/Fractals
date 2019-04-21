940d2d9e62246f91fb531d9803888b2a7a569bfa
Soumya Pattanayak <appxero@gmail.com>
2019-04-21 16:45:25 +0530

    Improved Supersampling.

--- OpenGLFractal/Source/Main.cpp
+++ OpenGLFractal/Source/Main.cpp
@@ -13,8 +13,8 @@ Color out = Color(245, 239, 239);				//	NOT IN SET		(LIGHTER)
 
 Color* LUT;
 
-int canvas_size = 1000;
-int supersampling = 1;
+int canvas_size = 800;
+int supersampling = 2;
 int iteration_limit = 1000;
 double zoom = 1;
 double pt_x = -0.761574;
@@ -35,16 +35,19 @@ Color Lerp(Color c1, Color c2, double currentStep)
 		clamp01(c1.g + (((c2.g - c1.g) / iteration_limit) * currentStep)),
 		clamp01(c1.b + (((c2.b - c1.b) / iteration_limit) * currentStep)));
 }
+std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-void threaded_render(Complex** cache, int** buffer, int it_count, int s_x, int s_y, int e_x, int e_y)
+void threaded_render(Complex** cache, int** buffer, int it_count, int s_x, int s_y, int e_x, int e_y, int thrd_count)
 {
 	double delta = 5 / (canvas_size * zoom * supersampling);																	// GET PIXEL SIZE WRT. SCALE
 	double x = pt_x - (canvas_size * supersampling / 2) * delta, y = -pt_y + (canvas_size * supersampling / 2) * delta;			// GET TOP-LEFT POSITION
 
+	int frame_c = 0;
 	for (int i = s_x; i < e_x; i++)
 	{
 		for (int j = s_y; j < e_y; j++)
 		{
+
 			double real = (cache[i][j].real * cache[i][j].real) - (cache[i][j].im * cache[i][j].im) + (x + i * delta);
 			double im = (2 * cache[i][j].im * cache[i][j].real) + (y - j * delta);
 
@@ -53,8 +56,12 @@ void threaded_render(Complex** cache, int** buffer, int it_count, int s_x, int s
 
 			cache[i][j].real = real;
 			cache[i][j].im = im;
+
+			frame_c++;
 		}
 	}
+
+//	std::cout << thrd_count << " ended." << std::endl;
 }
 
 void threaded_render_iterlaced(Complex** cache, int** buffer, int it_count, int offset)
@@ -103,8 +110,11 @@ void renderloop(GLFWwindow * window, int** buffer, bool refresh, bool fast)
 		int perthread = totalpixels / threads;
 
 		for (int i = 0; i < canvas_size * supersampling; i++)
+		{
+			if (i % supersampling != 0) continue;
 			for (int j = 0; j < canvas_size * supersampling; j++)
 			{
+				if (j % supersampling != 0) continue;
 				if ((j / supersampling) % 1 != 0) continue;
 				if (buffer[i][j] == -1)
 					glColor3d(in.r, in.g, in.b);
@@ -115,6 +125,7 @@ void renderloop(GLFWwindow * window, int** buffer, bool refresh, bool fast)
 				}
 				glVertex2d(i / supersampling, j / supersampling);
 			}
+		}
 		glEnd();
 		glFinish();
 
@@ -138,34 +149,34 @@ void pixel_iteration(Complex **cache, int** buffer, double scale, double s_x, do
 	switch (threads)
 	{
 	case 1:
-		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size ,       _d };
-		t1.join();
-		break;
-	case 2:
-		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size ,       _d };
-		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size , (2 * _d) };
-		t1.join();
-		t2.join();
-		break;
-	case 4:
-		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size,       _d };
-		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size, (2 * _d) };
-		t3 = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size, (3 * _d) };
-		t4 = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size, (4 * _d) };
-		t1.join();
-		t2.join();
-		t3.join();
-		t4.join();
-		break;
-	case 8:
-		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size,       _d };
-		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size, (2 * _d) };
-		t3 = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size, (3 * _d) };
-		t4 = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size, (4 * _d) };
-		t5 = std::thread{ threaded_render, cache, buffer, it_count, 0, 4 * _d, canvas_size, (5 * _d) };
-		t6 = std::thread{ threaded_render, cache, buffer, it_count, 0, 5 * _d, canvas_size, (6 * _d) };
-		t7 = std::thread{ threaded_render, cache, buffer, it_count, 0, 6 * _d, canvas_size, (7 * _d) };
-		t8 = std::thread{ threaded_render, cache, buffer, it_count, 0, 7 * _d, canvas_size, (8 * _d) };
+		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size * supersampling ,       _d, 1 };
+		t1.join();																		   
+		break;																			   
+	case 2:																				   
+		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size * supersampling ,       _d, 1 };
+		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size * supersampling , (2 * _d), 2 };
+		t1.join();																		   
+		t2.join();																		   
+		break;																			   
+	case 4:																				   
+		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size * supersampling,      _d,  1 };
+		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size * supersampling, (2 * _d), 2 };
+		t3 = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size * supersampling, (3 * _d), 3 };
+		t4 = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size * supersampling, (4 * _d), 4 };
+		t1.join();																		   
+		t2.join();																		   
+		t3.join();																		   
+		t4.join();																		   
+		break;																			   
+	case 8:																				   
+		t1 = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size * supersampling,      _d , 1};
+		t2 = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size * supersampling, (2 * _d), 2};
+		t3 = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size * supersampling, (3 * _d), 3};
+		t4 = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size * supersampling, (4 * _d), 4};
+		t5 = std::thread{ threaded_render, cache, buffer, it_count, 0, 4 * _d, canvas_size * supersampling, (5 * _d), 5};
+		t6 = std::thread{ threaded_render, cache, buffer, it_count, 0, 5 * _d, canvas_size * supersampling, (6 * _d), 6};
+		t7 = std::thread{ threaded_render, cache, buffer, it_count, 0, 6 * _d, canvas_size * supersampling, (7 * _d), 7};
+		t8 = std::thread{ threaded_render, cache, buffer, it_count, 0, 7 * _d, canvas_size * supersampling, (8 * _d), 8};
 		t1.join();
 		t2.join();
 		t3.join();
@@ -176,22 +187,22 @@ void pixel_iteration(Complex **cache, int** buffer, double scale, double s_x, do
 		t8.join();
 		break;
 	case 16:
-		t1  = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size,       _d };
-		t2  = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size, (2 * _d) };
-		t3  = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size, (3 * _d) };
-		t4  = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size, (4 * _d) };
-		t5  = std::thread{ threaded_render, cache, buffer, it_count, 0, 4 * _d, canvas_size, (5 * _d) };
-		t6  = std::thread{ threaded_render, cache, buffer, it_count, 0, 5 * _d, canvas_size, (6 * _d) };
-		t7  = std::thread{ threaded_render, cache, buffer, it_count, 0, 6 * _d, canvas_size, (7 * _d) };
-		t8  = std::thread{ threaded_render, cache, buffer, it_count, 0, 7 * _d, canvas_size, (8 * _d) };
-		t9  = std::thread{ threaded_render, cache, buffer, it_count, 0, 8 * _d, canvas_size, (9 * _d) };
-		t10 = std::thread{ threaded_render, cache, buffer, it_count, 0, 9 * _d, canvas_size, (10 * _d) };
-		t11 = std::thread{ threaded_render, cache, buffer, it_count, 0,10 * _d, canvas_size, (11 * _d) };
-		t12 = std::thread{ threaded_render, cache, buffer, it_count, 0,11 * _d, canvas_size, (12 * _d) };
-		t13 = std::thread{ threaded_render, cache, buffer, it_count, 0,12 * _d, canvas_size, (13 * _d) };
-		t14 = std::thread{ threaded_render, cache, buffer, it_count, 0,13 * _d, canvas_size, (14 * _d) };
-		t15 = std::thread{ threaded_render, cache, buffer, it_count, 0,14 * _d, canvas_size, (15 * _d) };
-		t16 = std::thread{ threaded_render, cache, buffer, it_count, 0,15 * _d, canvas_size, (16 * _d) };
+		t1  = std::thread{ threaded_render, cache, buffer, it_count, 0,      0, canvas_size * supersampling,       _d  , 1};
+		t2  = std::thread{ threaded_render, cache, buffer, it_count, 0,     _d, canvas_size * supersampling, (2 * _d)  , 2};
+		t3  = std::thread{ threaded_render, cache, buffer, it_count, 0, 2 * _d, canvas_size * supersampling, (3 * _d)  , 3};
+		t4  = std::thread{ threaded_render, cache, buffer, it_count, 0, 3 * _d, canvas_size * supersampling, (4 * _d)  , 4};
+		t5  = std::thread{ threaded_render, cache, buffer, it_count, 0, 4 * _d, canvas_size * supersampling, (5 * _d)  , 5};
+		t6  = std::thread{ threaded_render, cache, buffer, it_count, 0, 5 * _d, canvas_size * supersampling, (6 * _d)  , 6};
+		t7  = std::thread{ threaded_render, cache, buffer, it_count, 0, 6 * _d, canvas_size * supersampling, (7 * _d)  , 7};
+		t8  = std::thread{ threaded_render, cache, buffer, it_count, 0, 7 * _d, canvas_size * supersampling, (8 * _d)  , 8};
+		t9  = std::thread{ threaded_render, cache, buffer, it_count, 0, 8 * _d, canvas_size * supersampling, (9 * _d)  , 9};
+		t10 = std::thread{ threaded_render, cache, buffer, it_count, 0, 9 * _d, canvas_size * supersampling, (10 * _d) , 10 };
+		t11 = std::thread{ threaded_render, cache, buffer, it_count, 0,10 * _d, canvas_size * supersampling, (11 * _d) , 11 };
+		t12 = std::thread{ threaded_render, cache, buffer, it_count, 0,11 * _d, canvas_size * supersampling, (12 * _d) , 12 };
+		t13 = std::thread{ threaded_render, cache, buffer, it_count, 0,12 * _d, canvas_size * supersampling, (13 * _d) , 13 };
+		t14 = std::thread{ threaded_render, cache, buffer, it_count, 0,13 * _d, canvas_size * supersampling, (14 * _d) , 14 };
+		t15 = std::thread{ threaded_render, cache, buffer, it_count, 0,14 * _d, canvas_size * supersampling, (15 * _d) , 15 };
+		t16 = std::thread{ threaded_render, cache, buffer, it_count, 0,15 * _d, canvas_size * supersampling, (16 * _d) , 16 };
 		t1.join();
 		t2.join();
 		t3.join();
@@ -210,6 +221,8 @@ void pixel_iteration(Complex **cache, int** buffer, double scale, double s_x, do
 		t16.join();
 		break;
 	}
+
+//	std::cout << std::endl;
 }
 
 Color get(int** buffer, int x, int y)
@@ -282,6 +295,10 @@ void Redraw()
 			a[i][j].im = a[i][j].real = 0;
 		}
 	}
+
+	std::system("cls");
+	std::cout << "\nREDRAW INITIATED\n\n"; 
+	begin = std::chrono::steady_clock::now();
 }
 
 double l_x, l_y;
@@ -368,7 +385,6 @@ int main(void)
 		}
 	}
 
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -378,7 +394,7 @@ int main(void)
 		{
 			pixel_iteration(a, buffer, zoom, pt_x, pt_y, it++);
 			if (it % 50 == 0)
-				std::cout << "Iteration " << it << std::endl;
+				std::cout << "\rIteration " << it;
 
 			if (it > 2 && ((it < 10 && it % 2 == 0) || (it < 50 && it % 20 == 0) || (it < 100 && it % 25 == 0) || (it > 100 && it % 100 == 0)))
 				renderloop(window, buffer, 1, true);
@@ -390,11 +406,14 @@ int main(void)
 			if (!aa_complete)
 			{
 				std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-				std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()/1000 << std::endl;
+				std::cout << "\r" << iteration_limit << " iterations complete.\n";
+				std::cout << "Iteration time = " << (float)(std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count())/1000000 << " sec." << std::endl;
 				std::cout << "\nAntialiasing started." << std::endl;
 				antialias(window, 1, buffer, 1);
 				aa_complete = true;
-				std::cout << "\nAntialiasing complete." << std::endl;
+				std::chrono::steady_clock::time_point aa_end = std::chrono::steady_clock::now();
+				std::cout << "Antialiasing complete." << std::endl;
+				std::cout << "Antialiasing time = " << (float)(std::chrono::duration_cast<std::chrono::microseconds>(aa_end - end).count()) / 1000000 << " sec." << std::endl;
 			}
 			glfwPollEvents();
 		}
